007d5d76fb823746bfb580c2bd809a17af757bb0
romashovar <romashovar@gmail.com>
2026-05-08 13:24:12 +0200

    Поправил переключение по путям

--- current-path.js
+++ current-path.js
@@ -1,10 +1,10 @@
-function currentPath() {
+function currentPath() { //функция, которая печатает название текущего пути
 
   let path = localStorage.getItem('path');
 
-  if (path === "0") {document.getElementById("current-path").textContent = "Chronological";}
-  else if (path === "1") {document.getElementById("current-path").textContent = "Not Just Clothing";}
-  else if (path === "2") {document.getElementById("current-path").textContent = "Red Carpet";}
+  if (path === "0") {document.getElementById("current-path").textContent = "Path: Chronological";}
+  else if (path === "1") {document.getElementById("current-path").textContent = "Path: Not Just Clothing";}
+  else if (path === "2") {document.getElementById("current-path").textContent = "Path: Red Carpet";}
 }
 
 currentPath();
